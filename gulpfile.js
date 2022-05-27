70dd95c829c71ead34e2b9bdf103580769b20122
Douglas J. Carvalho <dev-douglas@outlook.com.br>
2022-05-27 19:03:19 -0300

    add htmlmin-gulp, colocando html src

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
-const gulp = require('gulp')
-const concat = require('gulp-concat')
-const cssmin = require('gulp-cssmin')
-const rename = require('gulp-rename')
-const uglify = require('gulp-uglify')
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const htmlmin = require('gulp-htmlmin');
+const { parallel } = require('gulp');
 
 function tarefasCSS(cb){
-   return gulp.src([
+    gulp.src([
        './node_modules/bootstrap/dist/css/bootstrap.css',
        './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
        './vendor/jquery-ui/jquery-ui.css',
@@ -19,10 +21,11 @@ function tarefasCSS(cb){
     .pipe(cssmin())
     .pipe(rename({suffix:'.min'}))
     .pipe(gulp.dest('./dist/css'))
+    cb()
 }
 
-function tarefasJS(){
-    return gulp.src([
+function tarefasJS(callback){
+     gulp.src([
         './node_modules/jquery/dist/jquery.js',
         './node_modules/bootstrap/dist/js/bootstrap.js',
         './node_modules/@fortawesome/fontawesome-free/js/fontawesome.js',
@@ -37,7 +40,16 @@ function tarefasJS(){
      .pipe(uglify())
      .pipe(rename({suffix:'.min'}))
      .pipe(gulp.dest('./dist/js'))
+     callback()
  }
+function tarefasHTML(callback){
+    gulp.src('./src/**/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./dist'))
+
+        callback()
+}
 
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
+exports.default = parallel( tarefasHTML, tarefasCSS, tarefasJS)
